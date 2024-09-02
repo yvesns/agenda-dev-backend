90a05bd54cac93aa44e5d93c9287ea52432b0388
yvesns <yves.ns2@gmail.com>
2024-09-02 15:01:23 -0300

    Update pessoa.service.ts

--- src/pessoa/service/pessoa/pessoa.service.ts
+++ src/pessoa/service/pessoa/pessoa.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { PessoaDto } from 'src/pessoa/dto/pessoa.dto/pessoa.dto';
 import { createClient } from '@supabase/supabase-js'
 
-const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3d2x1dnN0aHBqbXZqdXVyc2xoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcwMjg0NjIsImV4cCI6MjAxMjYwNDQ2Mn0.eC9fPng3kLaajJfmv-rm0rFXkJj9bV0Ny95PTFFsBSw'
-
 @Injectable()
 export class PessoaService {
     public pessoas: PessoaDto[] = [];
